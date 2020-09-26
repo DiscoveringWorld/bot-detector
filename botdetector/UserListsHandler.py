b680680430e4b8a700a1da5265da9d49e8fd7c46
fre3d0m <pupperfishh@gmail.com>
2020-09-26 15:53:11 -0400

    Change list to dictionary

--- botdetector/UserListsHandler.py
+++ botdetector/UserListsHandler.py
@@ -4,11 +4,11 @@ class UserListsHandler:
     friends_api_url = "https://friends.roblox.com/v1/users/{0}/friends"
     followings_api_url = "https://friends.roblox.com/v1/users/{0}/followings"
 
-    user_links = [
-        "followers",
-        "friends",
-        "followings"
-    ]
+    user_links = {
+        "followers": followers_api_url,
+        "friends": friends_api_url,
+        "followings": followings_api_url
+    }
 
     def __init__(self):
         pass
