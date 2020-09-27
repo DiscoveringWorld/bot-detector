b36d2156dd17740180b002f51838d56dd995c0c9
fre3d0m <pupperfishh@gmail.com>
2020-09-27 11:56:14 -0400

    Change Id to id

--- botdetector/User.py
+++ botdetector/User.py
@@ -10,4 +10,4 @@ class User():
         user_info = requester.get(users_api_url, True, None)
 
         self.username = username
-        self.user_id = user_info["Id"]
+        self.user_id = user_info["id"]
