2aeaebda289c8e98df1ac28448460625387110a6
fre3d0m <pupperfishh@gmail.com>
2020-09-27 11:55:00 -0400

    Change input to username

--- botdetector/User.py
+++ botdetector/User.py
@@ -2,12 +2,12 @@ from .ApiRequester import ApiRequester
 
 class User():
 
-    def __init__(self, user_id):
+    def __init__(self, username):
         
-        users_api_url = "https://api.roblox.com/users/{0}".format(user_id)
+        users_api_url = "https://api.roblox.com/users/get-by-username?username={0}".format(user_id)
 
         requester = ApiRequester()
         user_info = requester.get(users_api_url, True, None)
 
-        self.user_id = user_id
-        self.username = user_info["Username"]
+        self.username = username
+        self.user_id = user_info["Id"]
