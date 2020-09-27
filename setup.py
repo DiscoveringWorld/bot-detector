63a182544bc0a80fc5800c6a4dc2e8c87ce806b4
fre3d0m <pupperfishh@gmail.com>
2020-09-27 11:39:57 -0400

    Update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name = 'bot-detector',
-    version = '0.1.0',
+    version = '0.1.3',
     packages = ['botdetector'],
     entry_points = {
         'console_scripts': [
