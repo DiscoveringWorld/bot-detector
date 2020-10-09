e8bde7b86d5e705fe24fc71bf553f590266110ab
fre3d0m <pupperfishh@gmail.com>
2020-10-09 13:33:37 -0400

    Check success value and remove sleep call to remove the delay

--- botdetector/__main__.py
+++ botdetector/__main__.py
@@ -10,14 +10,23 @@ def main():
 
         if args[0] == 'run':
             print("- Initializing.. -")
-            time.sleep(2.5)
+
+            success = input_reader.read_args(args[0], args[1:])
+            
+            if success == False:
+                return
+            
             user_lists_handler = UserListsHandler()
             input_reader = InputReader()
+
+            time.sleep(2.5)
+
             print("- Running.. -")
-            time.sleep(2.5)
+            
             # list_handler.handle(args[2], args[1])
-            input_reader.read_args(args[0], args[1:])
+            
             time.sleep(2.5)
+            
             print(" - Done! -")
     except KeyboardInterrupt as e:
         pass
